58babfab637964874fc4b687267e64305fbe6a9e
Asyaoleynichenko <90897889+Asyaoleynichenko@users.noreply.github.com>
2022-06-29 17:13:07 +0300

    1

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -3,8 +3,10 @@ $( function() {
       collapsible: true
     });
   } );
-let cat = document.getElementById("cat")
-let dog = document.getElementById("dog")
+
+let box1 = document.getElementById("box1")
+let box2 = document.getElementById("box2")
+let box3 = document.getElementById("box3")
 
 let modal_window = document.getElementById("modal-window")
 let modal_img = document.getElementById("modal-window-image")
@@ -12,22 +14,39 @@ let modal_name = document.getElementById("modal-window-name")
 let modal_description = document.getElementById("modal-window-description")
 let modal_price = document.getElementById("modal-window-price")
 let modal_button = document.getElementById("modal-window-button")
-
+let congrats = document.getElementById("congrats")
 modal_button.onclick = function () {
     modal_window.style.display = "none"
+    congrats.style.opacity = "1"
+    congrats.animate(
+        [
+            { opacity: 1},
+            { opacity: 0}
+        ], {
+            duration: 3000,
+        }
+    ).play()
+    congrats.style.opacity = "0" 
 }
 
 
-cat.onclick = function() {
+box1.onclick = function() {
 
     modal_window.style.display = "block"
     modal_img.src = this.children[0].src
 
-    modal_name.innerHTML = "Cat"
+    modal_price.innerHTML = "400 РУБ"
 
-    modal_price.innerHTML = "100$"
+    modal_description.innerHTML = "футболка"//this.children[0].alt
+}
+box2.onclick = function() {
 
-    modal_description.innerHTML = this.children[0].alt
+    modal_window.style.display = "block"
+    modal_img.src = this.children[0].src
+
+    modal_price.innerHTML = "400 РУБ"
+
+    modal_description.innerHTML = "футболка"//this.children[0].alt
 }
 
 dog.onclick = function() {
